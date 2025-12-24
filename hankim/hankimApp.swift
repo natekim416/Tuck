d73b4f6b359c7ef645b23c92542183d38d5cd0bb
Nate <natekim416@gmail.com>
2025-12-23 23:10:46 -0500

    Initial Commit

---
+++ hankim/hankimApp.swift
@@ -0,0 +1,17 @@
+//
+//  hankimApp.swift
+//  hankim
+//
+//  Created by Nathaniel Kim on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct hankimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
